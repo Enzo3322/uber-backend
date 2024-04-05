7e358738d1ee4f8241d6db783ebcbe07c08c7a2a
Enzo3322 <enzo.spag14@gmail.com>
2024-04-05 19:00:38 -0300

    ensure read operations insert data before run

--- test/user.test.ts
+++ test/user.test.ts
@@ -25,16 +25,32 @@ test("Should create an account for the driver", async function () {
 });
 
 test("Should return an array of users", async function () {
+  const payload = {
+    name: "John Doe",
+    email: `john.doe${Math.random()}@gmail.com`,
+    cpf: "87748248800",
+    isPassenger: false,
+    carPlate: "ABC123",
+  };
+  const user = await jestE2E.post("/api/signup").send(payload);
+  expect(user.status).toBe(201);
   const output = await jestE2E.get("/api/users");
   expect(output.status).toBe(200);
   expect(output.body).toBeInstanceOf(Array);
 });
 
-test("Should return a passenger by accountId", async function () {
-  const users = await jestE2E.get("/api/users");
-  expect(users.status).toBe(200);
-  const [firstUser] = users.body;
-  const output = await jestE2E.get(`/api/users/${firstUser.account_id}`);
+test("Should return a user by accountId", async function () {
+  const payload = {
+    name: "John Doe",
+    email: `john.doe${Math.random()}@gmail.com`,
+    cpf: "87748248800",
+    isPassenger: false,
+    carPlate: "ABC123",
+  };
+  const user = await jestE2E.post("/api/signup").send(payload);
+  expect(user.status).toBe(201);
+  const { account_id } = user.body;
+  const output = await jestE2E.get(`/api/users/${account_id}`);
   expect(output.status).toBe(200);
   expect(output.body).toMatchObject(output.body);
 });
